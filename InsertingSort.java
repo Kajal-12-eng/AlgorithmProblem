f523d8ad2ab00a422fd64b986505c84b1fcb7462
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-23 05:40:30 +0530

    added Algorithm Problems file

---
+++ InsertingSort.java
@@ -0,0 +1,41 @@
+package com.bl;
+
+public class InsertingSort {
+	public static void main(String a[]) {
+		int[] myArray = { 860, 8, 200, 9 };
+
+		System.out.println("Before Insertion Sort");
+
+		printArray(myArray);
+
+		insertionSort(myArray);
+
+		System.out.println("After Insertion Sort");
+
+		printArray(myArray);
+	}
+
+	public static void insertionSort(int arr[]) {
+		int n = arr.length;
+
+		for (int i = 1; i < n; i++) {
+			int key = arr[i];
+			int j = i - 1;
+
+			while ((j > -1) && (arr[j] > key)) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = key;
+			printArray(arr);
+		}
+	}
+
+	static void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
